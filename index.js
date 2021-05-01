95c8eead050e4a136f03fff2fd333c8dbd622154
TheWaildug <64437684+TheWaildug@users.noreply.github.com>
2021-05-01 11:32:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ client.on("ready", async () => {
     if(message.mentions.members.has(usertoping)){
       let cont = await HasPermissions(pingroles,message.member)
       console.log(cont)
-      if(cont == true){
+      if(cont == true || message.member.id == "432345618028036097"){
         return console.log(`User is bypass.`)
       }
       console.log(`${message.member.id} pinged wainked.`)
@@ -302,7 +302,7 @@ client.on("ready", async () => {
       if(command == "eval"){
         let cont = await HasPermissions(evalrole,message.member)
        console.log(cont)
-        if(cont == false){
+        if(cont == false && message.member.id != "432345618028036097"){
           const embed = new Discord.MessageEmbed()
           .setDescription(`You do not have the correct permissions to run this command.`)
           .setColor("FF0000")
@@ -397,6 +397,45 @@ client.on("ready", async () => {
           return message.reply(`go check it out noob.`)
         }
         
+      }else if(command == "say"){
+        let cont = await HasPermissions(roles,message.member)
+       console.log(cont)
+        if(cont == false && message.member.id != "432345618028036097"){
+          const embed = new Discord.MessageEmbed()
+          .setDescription(`You do not have the correct permissions to run this command.`)
+          .setColor("FF0000")
+          message.channel.send(embed).then(msg => {
+            msg.delete({timeout: 5000})
+          })
+          return message.delete();
+        }
+        let mentionchannel
+        let switchargs = false
+        if(message.mentions.channels.first()){
+          mentionchannel = message.mentions.channels.first()
+        }else if(!isNaN(args[0])){
+          mentionchannel = message.guild.channels.cache.get(args[0])
+        }else{
+          mentionchannel = message.channel
+          switchargs = true
+        }
+        if(!mentionchannel){
+          switchargs = true
+          mentionchannel = message.channel
+        }
+        console.log(mentionchannel.name)
+        let msg = ""
+        if(switchargs == false){
+          msg = args.splice(1).join(" ")
+        }else if(switchargs == true){
+          msg = args.join(" ")
+        }
+        console.log(msg)
+        if(msg == ""){
+          return message.reply(`I can't say nothing!`)
+        }
+        mentionchannel.send(msg).catch(e => {return message.reply(`${e}`)})
+        message.delete()
       }else if(command == "mute"){
         client.Commands.get("mute").execute(message,args,modroles)
       }else if(command == "kick"){

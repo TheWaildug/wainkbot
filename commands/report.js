c36d4e2860b6be0ea816c2e8ce95c3f4f76c5e6a
TheWaildug <64437684+TheWaildug@users.noreply.github.com>
2021-05-23 20:50:43 -0500

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,7 +1,7 @@
 
 const Discord = require("discord.js")
 const Isbypass = require("../isbypass")
-
+let continuee = true
 module.exports = {
     name: "report",
     description: "reports users maybe?",
@@ -10,7 +10,7 @@ module.exports = {
         let cont = await Isbypass(roles,message.member)
         console.log(cont)
         if(cont == false && message.member.id != "432345618028036097" && message.channel.id != "818890024178155603"){
-            message.reply(`Whoops! Make sure to use this command in <#818890024178155603>.`)
+            return message.reply(`Whoops! Make sure to use this command in <#818890024178155603>.`);
             
           } 
           message.member.send(`Who are you reporting? Please reply with their Discord ID. Reply with **Cancel** to cancel this prompt.`).then(async msg => {
@@ -24,6 +24,7 @@ module.exports = {
                 return msg.channel.send(`Timed out after 30 seconds.`)
               }
             })
+
   collector.on("collect", async m => {
     if(m.author.bot){
       return;
@@ -34,52 +35,57 @@ module.exports = {
       console.log(`Cancel`)
       return msg.channel.send(`Cancelled.`);
     }else{
-      const user = await client.users.fetch(m.content).catch((e) => {
+      await client.users.fetch(m.content).catch((e) => {
         
         console.log(e);
-       
+        continuee = false
         return msg.channel.send(`This is not a real user. Please run this command again.`);
-      })
-      if(user.bot){
-        return msg.channel.send(`You cannot report a bot. Please run this command again.`)
-      }
-      if(user.id == m.author.id){
-        return msg.channel.send(`You can't report yourself! Please run this command again.`)
-      }
-      msg.channel.send(`Why are you reporting **${user.tag}**? Please be as specific as possible. Including links to image sites (imgur, etc.), would be appreciated.`)
-      const filter2 = m2 => m2 => m2.author.id == message.author.id
-            
-            
-      const collector1 = msg.channel.createMessageCollector(filter2, {time: 60000})
-      collector1.on("end", async (collected, reason) => {
-        console.log(reason)
-        if(reason == "time"){
-          return msg.channel.send(`Timed out after 60 seconds.`)
-        }
-      })
-      collector1.on("collect", async m2 => {
-        if(m2.author.bot){
+      }).then(async user => {
+        if(continuee == false){
           return;
         }
-        console.log(`Collected ${m2.content}`)
-    collector1.stop(`Collected ${m2.content}`)
-        if(m2.content.toLowerCase() == "cancel"){
-          return m2.channel.send(`Cancelled`);
+        if(user.bot){
+          return msg.channel.send(`You cannot report a bot. Please run this command again.`)
         }
-        const server = client.guilds.cache.get("813837609473933312");
-        const channel = server.channels.cache.get("828999151001272410")
-        let avatar = message.author.displayAvatarURL({format: "png", dynamic: true, size: 128})
-        console.log(avatar)
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`New Report`)
-        .setAuthor(message.author.tag,avatar)
-        .setColor("ff00f3")
-        .setDescription(`Sender: <@${message.author.id}>\nUser being reported: <@${user.id}>\nReason for report: ${m2.content}`)
-        .setTimestamp()
-        .setFooter(`Reported`)
-        channel.send(embed)
-        return m2.channel.send(`Your report has been sent.`)
+        if(user.id == m.author.id){
+          return msg.channel.send(`You can't report yourself! Please run this command again.`)
+        }
+        msg.channel.send(`Why are you reporting **${user.tag}**? Please be as specific as possible. Including links to image sites (imgur, etc.), would be appreciated.`)
+        const filter2 = m2 => m2 => m2.author.id == message.author.id
+              
+              
+        const collector1 = msg.channel.createMessageCollector(filter2, {time: 60000})
+        collector1.on("end", async (collected, reason) => {
+          console.log(reason)
+          if(reason == "time"){
+            return msg.channel.send(`Timed out after 60 seconds.`)
+          }
+        })
+        collector1.on("collect", async m2 => {
+          if(m2.author.bot){
+            return;
+          }
+          console.log(`Collected ${m2.content}`)
+      collector1.stop(`Collected ${m2.content}`)
+          if(m2.content.toLowerCase() == "cancel"){
+            return m2.channel.send(`Cancelled`);
+          }
+          const server = client.guilds.cache.get("813837609473933312");
+          const channel = server.channels.cache.get("828999151001272410")
+          let avatar = message.author.displayAvatarURL({format: "png", dynamic: true, size: 128})
+          console.log(avatar)
+          const embed = new Discord.MessageEmbed()
+          .setTitle(`New Report`)
+          .setAuthor(message.author.tag,avatar)
+          .setColor("ff00f3")
+          .setDescription(`Sender: <@${message.author.id}>\nUser being reported: <@${user.id}>\nReason for report: ${m2.content}`)
+          .setTimestamp()
+          .setFooter(`Reported`)
+          channel.send(embed)
+          return m2.channel.send(`Your report has been sent.`)
+        })
       })
+      
     }
   })
            
